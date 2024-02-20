b06f42eafb37ba8d8c85de57913fd2eee890a9ee
Oluwatimilehin <oluwatimilehintheophilus@gmail.com>
2024-02-21 00:24:09 +0100

    modified endpoint's response

--- authentication/views.py
+++ authentication/views.py
@@ -32,10 +32,19 @@ class SignUpView(generics.GenericAPIView):
 
             response = {
                 "message": "User Created Successfully",
-                "data": serializer.data
+                "data": {
+                    "user_data": serializer.data,
+                    "status": status.HTTP_201_CREATED,
+                }
             }
             return Response(data=response, status=status.HTTP_201_CREATED)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        response = {
+            "message": "User Not Created",
+            "error": serializer.errors,
+            "status": status.HTTP_400_BAD_REQUEST,
+        }
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CustomBackend(ModelBackend):
@@ -55,16 +64,15 @@ class SignInView(APIView):
     def get(self, request: Request):
         
         response = {
-            "user": str(request.user),
+            "message": "Welcome " + str(request.user),
+            "data": {
+                "user_data": str(request.user),
+                "status": status.HTTP_200_OK,
+            }
         }
-
         return Response(data=response, status=status.HTTP_200_OK)
 
     def post(self, request: Request):
-
-        # username_or_email = request.data.get('username_or_email')
-        # password = request.data.get('password')
-        # user = CustomBackend().authenticate(request=request, username=username_or_email, password=password)
 
         username = request.data.get('username') 
         password = request.data.get('password')
@@ -74,13 +82,20 @@ class SignInView(APIView):
         if user is not None:
             response = {
                 "message": "User signed in successfully",
-                "data": [user.username, user.email]
+                "data": {
+                    "user_data": {
+                        "email": user.email,
+                        "username": user.username,
+                    },
+                    "status": status.HTTP_200_OK,
+                }
             }
 
             return Response(data=response, status=status.HTTP_200_OK)
         else:
             response = {
-                "message": "Invalid credentials"
+                "message": "Invalid credentials",
+                "status": status.HTTP_401_UNAUTHORIZED,
                 }
             return Response(data=response, status=status.HTTP_401_UNAUTHORIZED)
 
@@ -98,14 +113,30 @@ class ChangePasswordView(generics.GenericAPIView):
             user = request.user
             if user.check_password(serializer.validated_data['old_password']):
                 if serializer.data.get('new_password') != serializer.data.get('confirm_password'):
-                    return Response(data={"error": "Password does not match"})
+                    response = {
+                        "message": "Password does not match",
+                        "error": serializer.errors,
+                        "status": status.HTTP_400_BAD_REQUEST,
+                    }
+                    return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
                 user.set_password(serializer.validated_data['new_password'])
                 user.save()
                 update_session_auth_hash(request, user)
                 
                 response = {
-                    "message": "Password has been changed successfully"
+                    "message": "Password has been changed successfully",
+                    "status": status.HTTP_200_OK,
                 }
                 return Response(data=response, status=status.HTTP_200_OK)
-            return Response(data={"error": "Old password is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
+            response = {
+                "message": "Old password is incorrect",
+                "error": serializer.errors,
+                "status": status.HTTP_400_BAD_REQUEST
+            }
+            return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+        response = {
+            "message": "Password not changed",
+            "error": serializer.errors,
+            "status": status.HTTP_400_BAD_REQUEST,
+        }
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
