07c7db129260623470c2683bff4702c15d7c3480
Timi <oluwatimilehintheophilus@gmail.com>
2024-11-16 01:03:02 +0100

    Revert commit merging into develop

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -44,6 +44,15 @@ class SignUpSerializer(serializers.ModelSerializer):
 
         return user
 
-class SignInSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)   
+
+#Serializer for handling password change/update
+
+class ChangePasswordSerializer(serializers.Serializer):
+
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(min_length=6, required=True)
+    confirm_password = serializers.CharField(required=True)
+
+    
+class ResetPasswordEmailSerializer(serializers.Serializer):
+    email = serializers.CharField(required=True)
--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
 from authentication.serializers import SignUpSerializer
-from authentication.serializers import SignInSerializer
 
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
@@ -72,30 +71,16 @@ class CustomBackend(ModelBackend):
 
 class SignInView(APIView):
 
-    serializer_class = SignInSerializer
-
-    def get(self, request: Request):
-        
-        response = {
-            "message": "Welcome " + str(request.user),
-            "data": {
-                "user_data": str(request.user),
-                "status": status.HTTP_200_OK,
-            }
-            
-        }
-
-        return Response(data=response, status=status.HTTP_200_OK)
-
+    @swagger_auto_schema(
+        operation_summary="SignIn a User",
+        operation_description="This endpoint authenticates and signin an existing user"
+    )
     def post(self, request: Request):
 
-        serializer = self.serializer_class(data=request.data)
+        username = request.data.get('username') 
+        password = request.data.get('password')
 
-        if serializer.is_valid():
-            username = serializer.validated_data['username']
-            password = serializer.validated_data['password']
-
-            user = CustomBackend().authenticate(request, username=username, password=password)
+        user = CustomBackend().authenticate(request, username=username, password=password)
 
         if user is not None:
 
